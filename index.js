d99c5ea911aaf61922c8754824b2208aa5512764
Serhii-SSD <demydenko.ss@gmail.com>
2026-06-28 17:10:46 +0000

    Deploying to gh-pages from @ Serhii-SSD/goit-js-hw-11@9b80810c3ddd9f80743d6773a48711ea83f80042 🚀

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import{a as p,S as u,i}from"./assets/vendor-CFFvTae-.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&o(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();async function f(a){const r="56484901-001e7e85bfee9cf80da113fbf",n="https://pixabay.com/api/";try{return(await p.get(n,{params:{key:r,q:a,image_type:"photo",orientation:"horizontal",safesearch:"true"}})).data.hits}catch(o){throw o}}const l=document.querySelector(".loader"),c=document.querySelector(".gallery");let m=new u(".gallery a",{captionsData:"alt",captionDelay:250});function d({webformatURL:a,largeImageURL:r,tags:n,likes:o,views:e,comments:t,downloads:s}){return`
 <li class="gallery-item">
 <a class="gallery-link" href="${r}">
- <img class="gallery-image" src="${a}" alt="${n}" loading="lazy" />
+<img class="gallery-image" src="${a}" alt="${n}" loading="lazy" />
 </a>
 <div class="info">
 <p class="info-item"><b>Likes</b><span>${o}</span></p>
